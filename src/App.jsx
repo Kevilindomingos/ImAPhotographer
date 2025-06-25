dcac9c52e5b833deb32c690578702c57af4fde5f
kevilin <kevilinmarcondes356@gmail.com>
2025-06-24 21:49:06 -0300

    to tentando mexer nesse css pra deixar show de bola

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,54 @@
-// import { useState } from 'react'
+
 import style from './App.module.css'
 import Menu from './components/menu'
+import menuImg from './assets/fotoHome.gif'
+
 
 function App() {
 
   return (
     <>
-    <section className={style.fundo}>
-      <div className={style.s1}>
-      <Menu/>
-       <div className={style.blocoDireito}></div>
-       <div></div>
-       <div></div>
+    <Menu />
+    <section id='s1' className={style.flexContainer}>
+      <div className={style.flexItem1}>
+        <img src={menuImg} className={style.menuImg} alt="foto inicial da home" />
+      </div>
+
+      <div className={style.flexItem2}>
+        <h1 className={style.ttHome}>Bem vindo ao meu site!</h1>
+        <p className={style.textHome}>Lorem ipsum dolor, sit amet consectetur adipisicing 
+          <br/>elit. Debitis facilis incidunt a animi, quaerat numquam 
+          quidem atque magnam qui eaque maxime delectus eligendi beatae
+           non esse sunt! Sit, provident doloremque.
+          <br/> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur
+            optio ab modi voluptas molestiae, voluptatibus consequatur commodi 
+            <br/>debitis provident magnam amet laudantium sint tempora reprehenderit,
+             quisquam porro laborum! Odit, tempora. Lorem ipsum, dolor sit amet consectetur
+              <br/>adipisicing elit. Labore quo optio eveniet saepe fugit quaerat similique rem
+              <br/> minus impedit eos, voluptate dolor quis. Molestiae atque voluptatem ipsam,
+                magnam in ab. <br/>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Harum blanditiis asperiores neque id vitae ex, dolores aperiam vel molestias 
+                <br/>voluptatibus nam, voluptate labore autem dolor qui commodi perferendis, necessitatibus repudiandae!</p>
       </div>
     </section>
+    <div className={style.flexBarra}>
+        <h4>A moldura do instante perfeito</h4>
+      </div>
+      <section id='s2' className={style.flexContainer2}>
+        <h1>conheça alguns exemplos da minha edição</h1>
+        <div>
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+      </section>
+      <section id='s3' className={style.flexContainer3}>
+
+      </section>
+      <footer id='footer' className={style.footer}>
+
+      </footer>
     </>
   )
 }
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,34 +1,14 @@
-// import './index.css'
+import style from './menu.module.css'
 
 function Menu() {
 
     return(
-        
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Início</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">meus trabalhos</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            mais opções
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">outros projetos</a></li>
-            <li><a class="dropdown-item" href="#">nao sei ainda</a></li>
-            <li><a class="dropdown-item" href="#">Redes sociais</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <header>
+        <a className={style.tituloheader} href='/'>HOME</a>
+        <a className={style.tituloheader} href='#s2'>SERVICES</a>
+        <a className={style.tituloheader} href='#s3'>ABOUT ME</a>
+        <a className={style.tituloheader} href='#footer'>CONTACT</a>
+      </header>
     )
 }
 
